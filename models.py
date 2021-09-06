707a8c1763e9349dca62e642471955e0725298db
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-09-06 13:11:04 +0530

    Update models.py

--- models.py
+++ models.py
@@ -1,5 +1,8 @@
+from django.dispatch import receiver
+from django.db.models.signals import pre_save, post_save
 from django.db import models
 from django.urls import reverse
+from django.utils import timezone
 from django.contrib.auth.models import User
 
 # Create your models here.
@@ -11,9 +14,10 @@ class VerificationStep(models.Model):
 	def __str__(self):
 		return self.name
 
+
 class WorkFlowModel(models.Model):
 	title       	= models.CharField(max_length=100)
-	steps			= models.ManyToManyField(VerificationStep)
+	steps			= models.ManyToManyField(VerificationStep, blank=True, null=True)
 	
 	class Meta:
 		ordering = ['title']
@@ -24,3 +28,30 @@ class WorkFlowModel(models.Model):
 	def __str__(self):
 		return f'{self.title}'
 
+
+class Document(models.Model):
+	doc_name 		= models.CharField(max_length=100, null=False)
+	doc_type 		= models.CharField(max_length=100, null=False)
+	work_flow 		= models.ForeignKey(WorkFlowModel, on_delete=models.CASCADE, null=True, default=None)
+	status 			= models.IntegerField(default=0)
+	status_name		= models.CharField(max_length=100, null=True)
+	update_time		= models.DateField()
+	notify_users 	= models.BooleanField(default=True)
+
+	def __str__(self):
+		return self.doc_name
+
+
+
+@receiver(pre_save, sender=Document)
+def document_pre_save(sender, instance, *args, **kwargs):
+	instance.updated_time = timezone.now()
+	if instance.status == 0:
+		instance.status = 1
+		instance.status_name = instance.work_flow.steps.all()[instance.status].name
+
+
+@receiver(post_save, sender=Document)
+def document_post_save(sender, instance, created, *args, **kwargs):
+	print('------------------Notifying Users', instance.work_flow.steps.all()[instance.status].authority )
+		
