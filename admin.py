57887e1485d03c48043f48c6111f4ad7b65643fd
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-09-06 13:09:40 +0530

    Update admin.py

--- admin.py
+++ admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import WorkFlowModel, VerificationStep
+from .models import WorkFlowModel, VerificationStep, Document
 
 # Register your models here.
 admin.site.register(WorkFlowModel)
-admin.site.register(VerificationStep)  
+admin.site.register(VerificationStep)
+admin.site.register(Document)  
