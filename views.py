c732cd5b0e5fb53a0763ff58dc54bee6335cfa4c
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-09-06 13:13:16 +0530

    Update views.py

--- views.py
+++ views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render, redirect, get_object_or_404
-
+from django.http import JsonResponse
+from django.utils import timezone
 from django.views import View
 from django.views.generic.edit import CreateView
 from django.views.generic.detail import DetailView
+from django.views.generic.list import ListView
 
-from products.models import Product
-from .models import WorkFlowModel
-from .utils import document_signing_process
+from .models import WorkFlowModel, Document
+from .forms import DocumentForm
 
 # Create your views here.
 
 class WorkFlowCreateView(CreateView):
 	model = WorkFlowModel
-	template_name = 'create.html'
+	template_name = 'workflow/create.html'
 	fields = [
 		'title', 'steps'
 	]
@@ -20,18 +21,48 @@ class WorkFlowCreateView(CreateView):
 
 class WorkFlowDetailView(DetailView):
 	model = WorkFlowModel
-	template_name = 'detail.html'
+	template_name = 'workflow/detail.html'
 
 	def get_object(self):
 		id_ = self.kwargs.get('id')
 		return get_object_or_404(WorkFlowModel, id=id_)
 
-class WorkFlowExecuteView(View):
+class DocumentWorkFlowAddView(View):
+	form = DocumentForm()
+
+	def get(self, request):
+		return render(request, 'workflow/execute.html', context={'form': self.form})
+
 	def post(self, request):
-		print(request.POST)
-		product_object = get_object_or_404(Product, id=request.POST['object_id'])
-		work_flow = get_object_or_404(WorkFlowModel, title=request.POST['wf_name'])
+		doc = Document(doc_name = request.POST['doc_name'],	doc_type = request.POST['doc_type'], work_flow = get_object_or_404(WorkFlowModel ,id=request.POST['work_flow']), notify_users = True)
+		doc.save()
+		return redirect('workflow:documents-in-workflow')
 
-		product_object = document_signing_process(product_object, work_flow)
-		return redirect('home')
 
+class DocumentExecutionListView(ListView):
+	model = Document
+	paginated_by = 10
+	
+
+class DocumentVerificationView(View):
+	def get(self, request, **kwargs):
+		id_ = kwargs.get('id')
+		obj = get_object_or_404(Document, id=id_)
+		return render(request, 'workflow/document_verify.html', { 'object': obj})
+
+	def put(self, request, **kwargs):
+		id_ = kwargs.get('id')
+		doc = get_object_or_404(Document, id=id_)
+
+		if(request.user == doc.work_flow.steps.all()[doc.status].authority):
+			status_value = doc.status + 1
+
+			if status_value >= len(doc.work_flow.steps.all()):
+				return JsonResponse({'Fully Verified': 'OK'})
+			else:
+				doc.status += 1
+				doc.status_name = doc.work_flow.steps.all()[doc.status].name
+				doc.save()
+				return JsonResponse({'Verified': 'OK'})
+		else:
+			return JsonResponse({'verified': 'NOT OK, You are NOT authorised'})
