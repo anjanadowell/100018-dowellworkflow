dac13ec757d56bde6b0f541fa24aad1c8ef99efd
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-09-13 04:05:50 +0530

    Update models.py

--- models.py
+++ models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 class SigningStep(models.Model):
-	name 		= models.TextField(max_length=100)
+	name 		= models.CharField(max_length=100)
 	authority	= models.ForeignKey(User, on_delete=models.CASCADE, null=False)
 
 	def __str__(self):
@@ -17,7 +17,7 @@ class SigningStep(models.Model):
 
 class WorkFlowModel(models.Model):
 	title       = models.CharField(max_length=100)
-	steps	    = models.ManyToManyField(SigningStep, blank=True)
+	steps		= models.ManyToManyField(SigningStep, blank=True)
 	
 	class Meta:
 		ordering = ['title']
@@ -28,22 +28,24 @@ class WorkFlowModel(models.Model):
 
 class DocumentType(models.Model):
 	title       			= models.CharField(max_length=100)
-	internal_work_flow 		= models.ForeignKey(WorkFlowModel, related_name='%(class)s_internal_wf', on_delete=models.SET_NULL, null=True, default=None)
-	external_work_flow 		= models.ForeignKey(WorkFlowModel, related_name='%(class)s_external_wf', on_delete=models.SET_NULL, null=True, default=None)
+	internal_work_flow 		= models.ForeignKey(WorkFlowModel, related_name='%(class)s_internal_wf', on_delete=models.SET_NULL, null=True, blank=True)
+	external_work_flow 		= models.ForeignKey(WorkFlowModel, related_name='%(class)s_external_wf', on_delete=models.SET_NULL, null=True, blank=True)
 
 
 	def get_absolute_url(self):
 		return reverse("workflow:detail", kwargs={"id": self.id})
 
 	def __str__(self):
-		return self.doc_name
+		return self.title
 
 class Document(models.Model):
 	document_name 		= models.CharField(max_length=100, null=False)
 	document_type 		= models.ForeignKey(DocumentType, on_delete=models.SET_NULL, null=True)
 	internal_status		= models.IntegerField(default=0)
+	internal_wf_step	= models.CharField(max_length=100, null=True, blank=True)
 	external_status		= models.IntegerField(default=0)
-	update_time		= models.DateField(null=True)
+	external_wf_step	= models.CharField(max_length=100, null=True, blank=True)
+	update_time			= models.DateField(null=True)
 	notify_users 		= models.BooleanField(default=True)
 
 	def __str__(self):
@@ -54,11 +56,19 @@ class Document(models.Model):
 @receiver(pre_save, sender=Document)
 def document_pre_save(sender, instance, *args, **kwargs):
 	instance.update_time = timezone.now()
-	# if instance.status_name :
-	# 	instance.status_name = instance.work_flow.steps.all()[instance.status].name
-
+	
+	if instance.document_type.internal_work_flow and instance.document_type.external_work_flow :
+		instance.internal_wf_step = instance.document_type.internal_work_flow.steps.all()[instance.internal_status].name
+		instance.external_wf_step = instance.document_type.external_work_flow.steps.all()[instance.external_status].name
+	elif instance.document_type.internal_work_flow :
+		instance.internal_wf_step = instance.document_type.internal_work_flow.steps.all()[instance.internal_status].name
+	elif instance.document_type.external_work_flow :
+		instance.external_wf_step = instance.document_type.external_work_flow.steps.all()[instance.external_status].name
+	else:
+		pass
 
 @receiver(post_save, sender=Document)
 def document_post_save(sender, instance, created, *args, **kwargs):
-	print('------------------Notifying Users', instance.work_flow.steps.all()[instance.status].authority )
+	print('------------------Notifying Users')
 		
+
