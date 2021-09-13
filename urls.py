a35ce93e44f120f486ee55009b8667c4d46a5520
Rahultadola <45748425+Rahultadola@users.noreply.github.com>
2021-09-13 15:08:38 +0530

    Update urls.py

--- urls.py
+++ urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.contrib.auth.decorators import login_required
-from .views import DocumentTypeCreateView, DocumentTypeDetailView, DocumentWorkFlowAddView, DocumentExecutionListView, DocumentVerificationView
+from .views import DocumentTypeCreateView, DocumentTypeListView, DocumentTypeDetailView, DocumentWorkFlowAddView, DocumentExecutionListView, DocumentVerificationView
 
 
 app_name = 'workflow'
 
 urlpatterns = [
-    path('create/', DocumentTypeCreateView.as_view(), name="create"),
-    path('detail/<int:id>', DocumentTypeDetailView.as_view(), name="detail"),
+    path('create-document-type/', DocumentTypeCreateView.as_view(), name="create-document-type"),
+    path('list-document-type/', DocumentTypeListView.as_view(), name="list-document-type"),
+    path('detail-document-type/<int:id>', DocumentTypeDetailView.as_view(), name="detail-document-type"),
     path('add-document/', DocumentWorkFlowAddView.as_view(), name="add-document"),
     path('documents-in-workflow/', DocumentExecutionListView.as_view(), name="documents-in-workflow"),
     path('verify-document/<int:id>', DocumentVerificationView.as_view(), name="verify-document")
